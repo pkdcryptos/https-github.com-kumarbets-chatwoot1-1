b14b8020e836a248e63b3db67c611908b7992d25
kumarbets <kumarbets@gmail.com>
2024-08-08 05:33:41 -0700

    removed lang, story, swagger related files

--- config/routes.rb
+++ config/routes.rb
@@ -515,11 +515,6 @@ Rails.application.routes.draw do
     post 'onboarding', to: 'onboarding#create'
   end
 
-  # ---------------------------------------------------------------------
-  # Routes for swagger docs
-  get '/swagger/*path', to: 'swagger#respond'
-  get '/swagger', to: 'swagger#respond'
-
   # ----------------------------------------------------------------------
   # Routes for testing
   resources :widget_tests, only: [:index] unless Rails.env.production?
