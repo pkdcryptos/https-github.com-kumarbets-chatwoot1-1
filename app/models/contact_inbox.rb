d9d6da82e752082fd16e71220933f55f3604633c
kumarbets <kumarbets@gmail.com>
2024-08-08 10:32:01 -0700

    source id fix

--- app/models/contact_inbox.rb
+++ app/models/contact_inbox.rb
@@ -26,7 +26,7 @@ class ContactInbox < ApplicationRecord
   validates :inbox_id, presence: true
   validates :contact_id, presence: true
   validates :source_id, presence: true
-  validate :valid_source_id_format?
+
 
   belongs_to :contact
   belongs_to :inbox
