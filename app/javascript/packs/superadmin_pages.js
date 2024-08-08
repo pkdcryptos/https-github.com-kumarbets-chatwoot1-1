ddbcedcecf139be6eb5d04f606b60d414459bd8f
kumarbets <kumarbets@gmail.com>
2024-08-08 02:03:43 -0700

    removed a few

--- app/javascript/packs/superadmin_pages.js
+++ app/javascript/packs/superadmin_pages.js
@@ -1,28 +1,17 @@
-import 'chart.js';
 import Vue from 'vue';
-import VueDOMPurifyHTML from 'vue-dompurify-html';
-Vue.use(VueDOMPurifyHTML);
 
-const PlaygroundIndex = () =>
-  import('../superadmin_pages/views/playground/Index.vue');
 
-const ComponentMapping = {
-  PlaygroundIndex: PlaygroundIndex,
-};
 
-const renderComponent = (componentName, props) => {
-  Vue.component(componentName, ComponentMapping[componentName]);
-  new Vue({
-    data: { props: props },
-    template: `<${componentName} :component-data="props"/>`,
+Vue.config.env = process.env;
+
+
+Vue.use(VueRouter);
+
+let template = "<div>Hello superadmin pages</div>"
+window.onload = () => {
+  window.WOOT = new Vue({
+    template,
   }).$mount('#app');
 };
 
-document.addEventListener('DOMContentLoaded', () => {
-  const element = document.getElementById('app');
-  if (element) {
-    const componentName = element.dataset.componentName;
-    const props = JSON.parse(element.dataset.props);
-    renderComponent(componentName, props);
-  }
-});
+
--- app/javascript/packs/survey.js
+++ app/javascript/packs/survey.js
@@ -1,26 +1,17 @@
 import Vue from 'vue';
-import VueI18n from 'vue-i18n';
-import App from '../survey/App.vue';
-import i18n from '../survey/i18n';
-import store from '../survey/store';
-import { emitter } from 'shared/helpers/mitt';
 
-Vue.use(VueI18n);
 
-const i18nConfig = new VueI18n({
-  locale: 'en',
-  messages: i18n,
-});
 
-// Event Bus
-Vue.prototype.$emitter = emitter;
+Vue.config.env = process.env;
 
-Vue.config.productionTip = false;
 
+Vue.use(VueRouter);
+
+let template = "<div>Hello survey</div>"
 window.onload = () => {
-  window.WOOT_SURVEY = new Vue({
-    i18n: i18nConfig,
-    store,
-    render: h => h(App),
+  window.WOOT = new Vue({
+    template,
   }).$mount('#app');
 };
+
+
