a0e9547653a336258f2a0ed15da880559bb62656
kumarbets <kumarbets@gmail.com>
2024-08-08 19:20:02 -0700

    SFSG SFSG tested worked - remove all files and start adding for functionality of - create 2 accounts, one inbox and start chatting and send attachments

--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,6 @@ ActiveRecord::Schema[7.0].define(version: 2024_05_16_003531) do
   enable_extension "pg_trgm"
   enable_extension "pgcrypto"
   enable_extension "plpgsql"    
-  
 
   create_table "access_tokens", force: :cascade do |t|
     t.string "owner_type"
