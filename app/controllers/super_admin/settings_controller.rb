80583ef8f9b055a5ad5747de6185b3d9628b65c6
kumarbets <kumarbets@gmail.com>
2024-08-10 16:21:22 -0700

    sfsg cleaned up app/controllers

--- app/controllers/super_admin/settings_controller.rb
+++
@@ -1,9 +0,0 @@
-class SuperAdmin::SettingsController < SuperAdmin::ApplicationController
-  def show; end
-
-  def refresh
-   # rubocop:disable Rails/I18nLocaleTexts
-    redirect_to super_admin_settings_path, notice: 'Instance status refreshed'
-    # rubocop:enable Rails/I18nLocaleTexts
-  end
-end
